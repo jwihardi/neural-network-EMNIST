b8a3fcd71a2252e62ccd13a5da712738f5c19e34
jwihardi <allarkvarkk@gmail.com>
2026-05-30 18:32:01 -0400

    starting point: copied serial-optimized -> parallel-cblas

---
+++ parallel-cblas/losses.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+
+#include "matrix.hpp"
+
+struct Losses{
+    static constexpr float EPSILON = 1e-7f; // the standard, using MIN_FLT would be too small
+
+    static inline float cross_entropy(const Matrix& predictions, int col, int label){
+        float p = predictions.data[label * predictions.cols + col];
+        p = std::max(p, EPSILON);
+        return -std::log(p);
+    }
+
+};
+
---
+++ parallel-cblas/matrix.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+#include <random>
+#include <cstdint>
+
+struct Dataset;
+
+struct Matrix{
+    std::vector<float> data;
+    int rows, cols;
+
+    Matrix(int, int);
+    static Matrix init_he(int, int, std::mt19937&);
+    static void load_image_into(const Dataset&, int, int, Matrix&);
+
+    void multiply_into(const Matrix&, Matrix&) const;
+    void transpose_multiply_into(const Matrix&, Matrix&) const;
+    void hadamard_into(const Matrix&, Matrix&) const;
+    
+
+    void add(const Matrix&);
+    void subtract_scaled(const Matrix&, float);
+    void subtract_outer_product(const Matrix&, const Matrix&, float);
+    void subtract_one_hot(const std::vector<uint8_t>&, int);
+
+    int argmax(int) const;
+};
