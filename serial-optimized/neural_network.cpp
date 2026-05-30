fbccafb084898f03a43fc69592eadff173e1dc15
jwihardi <allarkvarkk@gmail.com>
2026-05-30 04:09:29 -0400

    serial optimized completed for now, might clean up later

--- serial-optimized/neural_network.cpp
+++ serial-optimized/neural_network.cpp
@@ -8,21 +8,22 @@
 #include "../shared.hpp"
 
 void train(const Dataset&, Matrix&, Matrix&, Matrix&, Matrix&, int, float, int);
-void evaluate(const Dataset&, const Matrix&, const Matrix&, const Matrix&, const Matrix&);
+void evaluate(const Dataset&, const Matrix&, const Matrix&, const Matrix&, const Matrix&, int);
 
 int main(int argc, char *argv[]){
     std::cout << "Neural Network\n"; 
 
-    if(argc < 5){
+    if(argc < 6){
         std::cerr << "Usage: " << argv[0] 
-            << " <epochs> <learning rate> <hidden size> <batch_size>\n";
+            << " <epochs> <learning rate> <hidden size> <training_batch_size> <eval_batch_size>\n";
         return 1;
     }
 
     int epochs = std::stoi(argv[1]);
     float learning_rate = std::stof(argv[2]);
     int hidden_size = std::stoi(argv[3]);
-    int batch_size = std::stoi(argv[4]);
+    int training_batch_size = std::stoi(argv[4]);
+    int eval_batch_size = std::stoi(argv[5]);
 
     int num_labels = 10;
     Dataset dataset = load_images(Shared::IMAGE_FILE_PATH, num_labels);
@@ -38,13 +39,13 @@ int main(int argc, char *argv[]){
     Matrix b2(num_labels, 1);
 
     std::cout << "************\tTRAINING\t*************" << "\n";
-    train(dataset, W1, b1, W2, b2, epochs, learning_rate, batch_size);
+    train(dataset, W1, b1, W2, b2, epochs, learning_rate, training_batch_size);
    
     Dataset test_set = load_images(Shared::TEST_IMAGE_FILE_PATH, num_labels);
     load_labels(Shared::TEST_LABEL_FILE_PATH, &test_set);
 
     std::cout << "************\tEVALUATION\t*************\n";
-    evaluate(test_set, W1, b1, W2, b2);
+    evaluate(test_set, W1, b1, W2, b2, eval_batch_size);
 
     return 0;
 }
@@ -110,37 +111,44 @@ void train(const Dataset& dataset, Matrix& W1, Matrix& b1, Matrix& W2, Matrix& b
     }
 }
 
-void evaluate(const Dataset& dataset, const Matrix& W1, const Matrix& b1, const Matrix& W2, const Matrix& b2){
+void evaluate(const Dataset& dataset, const Matrix& W1, const Matrix& b1, const Matrix& W2, const Matrix& b2, int batch_size){
     int input_size = dataset.height * dataset.width;
     int hidden_size = W1.rows;
     int num_labels = W2.rows;
 
-    Matrix x(input_size, 1);
-    Matrix z1(hidden_size, 1);
-    Matrix a1(hidden_size, 1);
-    Matrix z2(num_labels, 1);
-    Matrix predictions(num_labels, 1);
+    Matrix X(input_size, batch_size);
+    Matrix Z1(hidden_size, batch_size);
+    Matrix A1(hidden_size, batch_size);
+    Matrix Z2(num_labels, batch_size);
+    Matrix predictions(num_labels, batch_size);
 
     float tot_loss = 0.0f;
     int correct = 0;
+    
+    // assuming divide evenly
+    const unsigned int num_batches = static_cast<int>(dataset.num_samples) / batch_size;
 
-    for(unsigned int sample = 0; sample < dataset.num_samples; sample++){
-        Matrix::load_image_into(dataset, sample, 1, x);
+    for(unsigned int batch = 0; batch < num_batches; batch++){
+        int start = batch * batch_size;
+        Matrix::load_image_into(dataset, start, batch_size, X);
+        
+        W1.multiply_into(X, Z1);
+        Z1.add(b1);
+        Activations::relu_into(Z1, A1);
+        
+        W2.multiply_into(A1, Z2);
+        Z2.add(b2);
+        Activations::softmax_into(Z2, predictions);
 
-        W1.multiply_into(x, z1);
-        z1.add(b1);
-        Activations::relu_into(z1, a1);
-
-        W2.multiply_into(a1, z2);
-        z2.add(b2);
-
-        Activations::softmax_into(z2, predictions);
-
-        int actual = dataset.labels[sample];
-        if(predictions.argmax(0) == actual) ++correct;
-        tot_loss += Losses::cross_entropy(predictions, 0, actual);
+        for(int i = 0; i < batch_size; i++){
+            int actual = dataset.labels[start + i];
+            if(predictions.argmax(i) == actual) ++correct;
+            tot_loss += Losses::cross_entropy(predictions, i, actual);
+        }
     }
-    float avg_loss = tot_loss / static_cast<float>(dataset.num_samples);
-    float accuracy = static_cast<float>(correct) / static_cast<float>(dataset.num_samples);
+    // safegaurd incase the assumed isn't true
+    const float processed = static_cast<float>(num_batches * batch_size);
+    float avg_loss = tot_loss / processed;
+    float accuracy = static_cast<float>(correct) / processed;
     std::cout << "test loss: " << avg_loss << " | test accuracy: " << accuracy << "\n";
 }
